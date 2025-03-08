4ba57ad8bc8e693d402d2d65aed6f5d1c39b15d1
SAMADHI PRARTHANA <165768893+samadhiprarthana@users.noreply.github.com>
2025-03-08 09:17:32 +0530

    frontend start

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>This is a simple Todo App built with MERN Stack.</p>
+    </div>
+  );
+}
+
+export default About;
